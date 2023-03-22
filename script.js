a52bc2f5f0c7ed460e69f8ff4fb7cf0521e7b84d
Lucasbandrade <90878516+Lucasbandrade@users.noreply.github.com>
2023-03-22 18:31:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function adicionarFilme() {
+  var campoFilmeFavorito = document.getElementById("filmes").value;
+  var elementoFilmeFavorito = "<img src=" + campoFilmeFavorito + ">";
+  var listaFilmes = document.gerElementById("listaFilmes");
+}
